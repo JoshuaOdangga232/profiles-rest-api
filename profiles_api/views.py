a4409f71d415789f08b33bcd70b060c3e415c5ff
josh <joshuaodangga4@gmail.com>
2026-03-18 08:30:46 +0800

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,12 +6,13 @@ class HelloApiView(APIView):
     """Test API View"""
 
     def get(self, request, format=None):
-    """Returns a list of APIView features"""
-    an_apiview = [
-         'Uses HTTP methods as function (get, post, patch, put, delete)',
-         'Is similar to a traditional Django View',
-         'Gives you the most control over you application logic',
-         'Is mapped manually to URLs',
-    ]
+        """Returns a list of APIView features"""
 
-    return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
